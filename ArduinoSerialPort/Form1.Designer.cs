8fa5107f751f5b589cb13ab7edf99cec0bd524cc
Çağrı ÇİMENCİ <cagricimenci@gmail.com>
2018-07-24 11:00:33 +0300

    Project added

---
+++ ArduinoSerialPort/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ArduinoSerialPort
+{
+    partial class SerialPortRead
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.picChane = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picChane)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connection Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Connection  Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(300, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(300, 54);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Enabled = false;
+            this.txtStatus.Location = new System.Drawing.Point(427, 26);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(100, 20);
+            this.txtStatus.TabIndex = 7;
+            // 
+            // txtData
+            // 
+            this.txtData.Enabled = false;
+            this.txtData.Location = new System.Drawing.Point(35, 105);
+            this.txtData.Name = "txtData";
+            this.txtData.ReadOnly = true;
+            this.txtData.Size = new System.Drawing.Size(100, 20);
+            this.txtData.TabIndex = 8;
+            // 
+            // picChane
+            // 
+            this.picChane.Location = new System.Drawing.Point(300, 105);
+            this.picChane.Name = "picChane";
+            this.picChane.Size = new System.Drawing.Size(669, 437);
+            this.picChane.TabIndex = 9;
+            this.picChane.TabStop = false;
+            // 
+            // SerialPortRead
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 554);
+            this.Controls.Add(this.picChane);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "SerialPortRead";
+            this.Text = "SerialPortRead";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picChane)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.PictureBox picChane;
+    }
+}
+
---
+++ ArduinoSerialPort/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+
+namespace ArduinoSerialPort
+{
+    public partial class SerialPortRead : Form
+    {
+        string[] portlar = SerialPort.GetPortNames();
+
+        public SerialPortRead()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            foreach (string port in portlar)
+            {
+                comboBox1.Items.Add(port);
+                comboBox1.SelectedIndex = 0;
+            }
+            comboBox2.Items.Add("4800");
+            comboBox2.Items.Add("9600");
+            comboBox2.SelectedIndex = 1;
+            txtStatus.Text = "Bağlantı kapalı";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                string sonuc = serialPort1.ReadExisting();
+                txtData.Text = sonuc;
+
+                switch (txtData.Text)
+                {
+                    case "0": picChane.Image = Properties.Resources.videoplayback___2of2_01; break;
+                    case "1": picChane.Image = Properties.Resources.videoplayback___2of2_02; break;
+                    case "2": picChane.Image = Properties.Resources.videoplayback___2of2_03; break;
+                    case "3": picChane.Image = Properties.Resources.videoplayback___2of2_04; break;
+                    case "4": picChane.Image = Properties.Resources.videoplayback___2of2_05; break;
+                    case "5": picChane.Image = Properties.Resources.videoplayback___2of2_06; break;
+                    case "6": picChane.Image = Properties.Resources.videoplayback___2of2_07; break;
+                    case "7": picChane.Image = Properties.Resources.videoplayback___2of2_08; break;
+                    case "8": picChane.Image = Properties.Resources.videoplayback___2of2_09; break;
+                    case "9": picChane.Image = Properties.Resources.videoplayback___2of2_10; break;
+                    case "10": picChane.Image = Properties.Resources.videoplayback___2of2_11; break;
+                    case "11": picChane.Image = Properties.Resources.videoplayback___2of2_12; break;
+                    case "12": picChane.Image = Properties.Resources.videoplayback___2of2_13; break;
+                    case "13": picChane.Image = Properties.Resources.videoplayback___2of2_14; break;
+                    case "14": picChane.Image = Properties.Resources.videoplayback___2of2_15; break;
+                    case "15": picChane.Image = Properties.Resources.videoplayback___2of2_16; break;
+                    case "16": picChane.Image = Properties.Resources.videoplayback___2of2_17; break;
+                    case "17": picChane.Image = Properties.Resources.videoplayback___2of2_18; break;
+                    case "18": picChane.Image = Properties.Resources.videoplayback___2of2_19; break;
+                    case "19": picChane.Image = Properties.Resources.videoplayback___2of2_20; break;
+                    case "20": picChane.Image = Properties.Resources.videoplayback___2of2_21; break;
+                    case "21": picChane.Image = Properties.Resources.videoplayback___2of2_22; break;
+                    case "22": picChane.Image = Properties.Resources.videoplayback___2of2_23; break;
+                    case "23": picChane.Image = Properties.Resources.videoplayback___2of2_24; break;
+                    case "24": picChane.Image = Properties.Resources.videoplayback___2of2_25; break;
+                    case "25": picChane.Image = Properties.Resources.videoplayback___2of2_26; break;
+                    case "26": picChane.Image = Properties.Resources.videoplayback___2of2_27; break;
+                    case "27": picChane.Image = Properties.Resources.videoplayback___2of2_28; break;
+                    case "28": picChane.Image = Properties.Resources.videoplayback___2of2_29; break;
+                    case "29": picChane.Image = Properties.Resources.videoplayback___2of2_30; break;
+                    case "30": picChane.Image = Properties.Resources.videoplayback___2of2_31; break;
+                    case "31": picChane.Image = Properties.Resources.videoplayback___2of2_32; break;
+                    case "32": picChane.Image = Properties.Resources.videoplayback___2of2_33; break;
+                    case "33": picChane.Image = Properties.Resources.videoplayback___2of2_34; break;
+                    case "34": picChane.Image = Properties.Resources.videoplayback___2of2_35; break;
+                    case "35": picChane.Image = Properties.Resources.videoplayback___2of2_36; break;
+                    case "36": picChane.Image = Properties.Resources.videoplayback___2of2_37; break;
+                    case "37": picChane.Image = Properties.Resources.videoplayback___2of2_38; break;
+                    case "38": picChane.Image = Properties.Resources.videoplayback___2of2_39; break;
+                    case "39": picChane.Image = Properties.Resources.videoplayback___2of2_40; break;
+
+                }
+          
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                timer1.Stop();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            if (serialPort1.IsOpen == false)
+            {
+                if (comboBox1.Text == "")
+                    return;
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt16(comboBox2.Text);
+                try
+                {
+                    serialPort1.Open();
+                    txtStatus.Text = "Bağlantı açık";
+                }
+                catch (Exception hata)
+                {
+                    MessageBox.Show("hata:" + hata.Message);
+                }
+            }
+            else
+            {
+                txtStatus.Text = "bağlantı ok";
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+                txtStatus.Text = "bağlantı kapalı";
+            }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if
+                (serialPort1.IsOpen == true)
+                serialPort1.Close();
+        }
+    }
+    internal class serialPort1
+    {
+
+
+    }
+}
+
